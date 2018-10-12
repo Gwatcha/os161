27bba5a8a91dbcc18ad883ad87dc73880f2fecf9
ian <ianmccall93@gmail.com>
2018-10-11 20:03:48 -0700

    switch-case stubs for assignment 4 system calls

--- kern/arch/mips/syscall/syscall.c
+++ kern/arch/mips/syscall/syscall.c
@@ -109,7 +109,29 @@ syscall(struct trapframe *tf)
 				 (userptr_t)tf->tf_a1);
 		break;
 
-	    /* Add stuff here */
+            case SYS_open:
+                break;
+
+            case SYS_read:
+                break;
+
+            case SYS_write:
+                break;
+
+            case SYS_lseek:
+                break;
+
+            case SYS_close:
+                break;
+
+            case SYS_dup2:
+                break;
+
+            case SYS_chdir:
+                break;
+
+            case SYS___getcwd:
+                break;
 
 	    default:
 		kprintf("Unknown syscall %d\n", callno);
