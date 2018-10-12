392451b249eaabd8e7326b0a9a14b2ec933b787d
ian <ianmccall93@gmail.com>
2018-10-11 20:02:07 -0700

    Better indentation for c-switch statements in emacs editor

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,5 @@
 ((nil . ((tab-width . 8)
-         (c-basic-offset . 8))))
+         (c-basic-offset . 8)
+         (c-file-offsets
+          (case-label . *)
+          (statement-case-intro . *)))))
