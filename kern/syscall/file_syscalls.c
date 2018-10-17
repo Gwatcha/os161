348eb8e82b8f04f3a4585935861466c9518fffb1
ian <ianmccall93@gmail.com>
2018-10-17 14:01:17 -0700

    Braces for if statement

--- kern/syscall/file_syscalls.c
+++ kern/syscall/file_syscalls.c
@@ -121,8 +121,9 @@ int sys_read(ssize_t * retval, int fd, void* buf, size_t buflen)
         struct file_table_entry** file_table = curproc->p_file_table;
 
         /* bad fd checks */
-        if (fd < 0 || fd >= __OPEN_MAX)
+        if (fd < 0 || fd >= __OPEN_MAX) {
     		return EBADF;
+        }
 
         if (file_table[fd] == NULL) {
 		return EBADF;
