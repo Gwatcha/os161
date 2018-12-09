6b2f3d70a6a76248063aaa7437e03af6f3a94a57
gwatcha <michael.ampm@gmail.com>
2018-12-08 17:32:48 -0800

    move address space functions to bottom of vm.c

--- kern/arch/mips/vm/vm.c
+++ kern/arch/mips/vm/vm.c
@@ -379,6 +379,183 @@ vm_fault(int faulttype, vaddr_t faultaddress)
 	return EFAULT;
 }
 
+
+static
+void
+reserve_vpage(page_table* pt, vpage_t vpage)
+{
+        KASSERTM(!page_table_contains(pt, vpage),
+                 "Page table already contains an entry for %x", vpage);
+
+        DEBUG(DB_VM, "vm: reserve vpage 0x%x\n", vpage);
+
+        /*
+         * Write an invalid ppage:
+         * actual page frames are allocated when the memory is accessed
+         */
+        page_table_write(pt, vpage, PPAGE_INVALID);
+}
+
+
+static
+ppage_t
+copy_to_new_page(ppage_t old_page)
+{
+        if (old_page == PPAGE_INVALID) {
+                return PPAGE_INVALID;
+        }
+
+        const ppage_t new_page = claim_free_pages(1);
+        if (new_page == PPAGE_INVALID) {
+                return PPAGE_INVALID;
+        }
+
+        const vaddr_t old_address = PADDR_TO_KVADDR(page_to_addr(old_page));
+        const vaddr_t new_address = PADDR_TO_KVADDR(page_to_addr(new_page));
+
+        DEBUG(DB_VM, "vm: copy page 0x%x -> 0x%x\n", old_page, new_page);
+
+        memcpy((void*)new_address, (const void*)old_address, PAGE_SIZE);
+
+        DEBUG(DB_VM, "vm: done copy page\n");
+
+        return new_page;
+}
+
+// ~~~~~~~~~~~ Address Space Functionality ~~~~~~~~~~~
+
+static
+UNUSED
+void
+as_zero_region(paddr_t paddr, unsigned npages)
+{
+	bzero((void *)PADDR_TO_KVADDR(paddr), npages * PAGE_SIZE);
+}
+
+int
+as_prepare_load(struct addrspace *as)
+{
+        (void)as;
+	return 0;
+}
+
+int
+as_complete_load(struct addrspace *as)
+{
+	(void)as;
+	return 0;
+}
+
+int
+as_define_stack(struct addrspace *as, vaddr_t *stackptr)
+{
+        DEBUG(DB_VM, "vm: as_define_stack()\n");
+
+        page_table* pt = &as->as_page_table;
+
+        const vpage_t stack_top = addr_to_page(USERSTACK);
+        const vpage_t stack_bottom = stack_top - DUMBVM_STACKPAGES;
+
+        /* Reserve virtual pages for the stack */
+        for (vpage_t vpage = stack_top; vpage > stack_bottom; --vpage) {
+
+                reserve_vpage(pt, vpage);
+        }
+
+	*stackptr = USERSTACK;
+
+        DEBUG(DB_VM, "vm: as_define_stack() done\n");
+	return 0;
+}
+
+int
+as_copy(struct addrspace* old, struct addrspace** ret)
+{
+        DEBUG(DB_VM, "vm: as_copy()\n");
+
+        *ret = as_create();
+        if (*ret == NULL) {
+                return ENOMEM;
+        }
+        page_table* new_pt = &(*ret)->as_page_table;
+
+        const page_table* old_pt = &old->as_page_table;
+        const page_mapping* old_mappings = old_pt->pt_mappings;
+        const unsigned old_capacity = old_pt->pt_capacity;
+
+        for (unsigned i = 0; i < old_capacity; ++i) {
+
+                const page_mapping* old_mapping = old_mappings + i;
+
+                if (!page_mapping_is_valid(old_mapping)) {
+                        continue;
+                }
+
+                const vpage_t old_vpage = old_mapping->pm_vpage;
+                const ppage_t old_ppage = old_mapping->pm_ppage;
+
+                const ppage_t new_ppage = copy_to_new_page(old_ppage);
+
+                /*
+                 * TODO: if the old page is valid and the new page is invalid
+                 * then we have run out of pages
+                 */
+
+                page_table_write(new_pt, old_vpage, new_ppage);
+        }
+
+        DEBUG(DB_VM, "vm: as_copy() done\n");
+
+	return 0;
+}
+
+/*
+ * Set up a segment at virtual address VADDR of size MEMSIZE. The
+ * segment in memory extends from VADDR up to (but not including)
+ * VADDR+MEMSIZE.
+ *
+ * The READABLE, WRITEABLE, and EXECUTABLE flags are set if read,
+ * write, or execute permission should be set on the segment. At the
+ * moment, these are ignored. When you write the VM system, you may
+ * want to implement them.
+ */
+int
+as_define_region(struct addrspace *as, vaddr_t vaddr, size_t size,
+		 int readable, int writeable, int executable)
+{
+        KASSERT(as != NULL);
+
+        DEBUG(DB_VM,
+              "vm: as_define_region(vaddr:      0x%08x\n"
+              "                     size:       0x%08x\n"
+              "                     readable:   %d\n"
+              "                     writeable:  %d\n"
+              "                     executable: %d\n"
+              ")\n",
+              vaddr, size, readable, writeable, executable);
+
+	/* Not using these yet - all pages are read-write */
+	(void)readable;
+	(void)writeable;
+	(void)executable;
+
+        page_table* pt = &as->as_page_table;
+
+        const vaddr_t vaddr_max = vaddr + size;
+
+        const vpage_t vpage_min = addr_to_page(vaddr);
+        const vpage_t vpage_max = addr_to_page(vaddr_max);
+
+        /* Reserve virtual pages for the region */
+        for (vpage_t vpage = vpage_min; vpage <= vpage_max; ++vpage) {
+
+                reserve_vpage(pt, vpage);
+        }
+
+        DEBUG(DB_VM, "vm: as_define_region() done\n");
+
+        return 0;
+}
 struct addrspace *
 as_create(void)
 {
@@ -437,168 +614,4 @@ void
 as_deactivate(void)
 {
 	/* nothing */
-}
-
-static
-void
-reserve_vpage(page_table* pt, vpage_t vpage)
-{
-        KASSERTM(!page_table_contains(pt, vpage),
-                 "Page table already contains an entry for %x", vpage);
-
-        DEBUG(DB_VM, "vm: reserve vpage 0x%x\n", vpage);
-
-        /*
-         * Write an invalid ppage:
-         * actual page frames are allocated when the memory is accessed
-         */
-        page_table_write(pt, vpage, PPAGE_INVALID);
-}
-
-int
-as_define_region(struct addrspace *as, vaddr_t vaddr, size_t size,
-		 int readable, int writeable, int executable)
-{
-        KASSERT(as != NULL);
-
-        DEBUG(DB_VM,
-              "vm: as_define_region(vaddr:      0x%08x\n"
-              "                     size:       0x%08x\n"
-              "                     readable:   %d\n"
-              "                     writeable:  %d\n"
-              "                     executable: %d\n"
-              ")\n",
-              vaddr, size, readable, writeable, executable);
-
-	/* Not using these yet - all pages are read-write */
-	(void)readable;
-	(void)writeable;
-	(void)executable;
-
-        page_table* pt = &as->as_page_table;
-
-        const vaddr_t vaddr_max = vaddr + size;
-
-        const vpage_t vpage_min = addr_to_page(vaddr);
-        const vpage_t vpage_max = addr_to_page(vaddr_max);
-
-        /* Reserve virtual pages for the region */
-        for (vpage_t vpage = vpage_min; vpage <= vpage_max; ++vpage) {
-
-                reserve_vpage(pt, vpage);
-        }
-
-        DEBUG(DB_VM, "vm: as_define_region() done\n");
-
-        return 0;
-}
-
-static
-UNUSED
-void
-as_zero_region(paddr_t paddr, unsigned npages)
-{
-	bzero((void *)PADDR_TO_KVADDR(paddr), npages * PAGE_SIZE);
-}
-
-int
-as_prepare_load(struct addrspace *as)
-{
-        (void)as;
-	return 0;
-}
-
-int
-as_complete_load(struct addrspace *as)
-{
-	(void)as;
-	return 0;
-}
-
-int
-as_define_stack(struct addrspace *as, vaddr_t *stackptr)
-{
-        DEBUG(DB_VM, "vm: as_define_stack()\n");
-
-        page_table* pt = &as->as_page_table;
-
-        const vpage_t stack_top = addr_to_page(USERSTACK);
-        const vpage_t stack_bottom = stack_top - DUMBVM_STACKPAGES;
-
-        /* Reserve virtual pages for the stack */
-        for (vpage_t vpage = stack_top; vpage > stack_bottom; --vpage) {
-
-                reserve_vpage(pt, vpage);
-        }
-
-	*stackptr = USERSTACK;
-
-        DEBUG(DB_VM, "vm: as_define_stack() done\n");
-	return 0;
-}
-
-static
-ppage_t
-copy_to_new_page(ppage_t old_page)
-{
-        if (old_page == PPAGE_INVALID) {
-                return PPAGE_INVALID;
-        }
-
-        const ppage_t new_page = claim_free_pages(1);
-        if (new_page == PPAGE_INVALID) {
-                return PPAGE_INVALID;
-        }
-
-        const vaddr_t old_address = PADDR_TO_KVADDR(page_to_addr(old_page));
-        const vaddr_t new_address = PADDR_TO_KVADDR(page_to_addr(new_page));
-
-        DEBUG(DB_VM, "vm: copy page 0x%x -> 0x%x\n", old_page, new_page);
-
-        memcpy((void*)new_address, (const void*)old_address, PAGE_SIZE);
-
-        DEBUG(DB_VM, "vm: done copy page\n");
-
-        return new_page;
-}
-
-int
-as_copy(struct addrspace* old, struct addrspace** ret)
-{
-        DEBUG(DB_VM, "vm: as_copy()\n");
-
-        *ret = as_create();
-        if (*ret == NULL) {
-                return ENOMEM;
-        }
-        page_table* new_pt = &(*ret)->as_page_table;
-
-        const page_table* old_pt = &old->as_page_table;
-        const page_mapping* old_mappings = old_pt->pt_mappings;
-        const unsigned old_capacity = old_pt->pt_capacity;
-
-        for (unsigned i = 0; i < old_capacity; ++i) {
-
-                const page_mapping* old_mapping = old_mappings + i;
-
-                if (!page_mapping_is_valid(old_mapping)) {
-                        continue;
-                }
-
-                const vpage_t old_vpage = old_mapping->pm_vpage;
-                const ppage_t old_ppage = old_mapping->pm_ppage;
-
-                const ppage_t new_ppage = copy_to_new_page(old_ppage);
-
-                /*
-                 * TODO: if the old page is valid and the new page is invalid
-                 * then we have run out of pages
-                 */
-
-                page_table_write(new_pt, old_vpage, new_ppage);
-        }
-
-        DEBUG(DB_VM, "vm: as_copy() done\n");
-
-	return 0;
 }
